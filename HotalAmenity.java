93eda603f0f50c39ab863230c0fb2fe3a735f0fa
MostafaMohAli <MOSTAFA@email.sc.edu>
2022-03-15 12:31:24 -0400

    update

---
+++ HotalAmenity.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ * @author Mostafa Mohamed Ali
+ */
+public enum HotalAmenity {
+    PET_FRIENDLY, STAIR_FREE_PATH_TO_ENTRANCE, PARKING, FREE_WIFI, POOL, AIR_CONDITIONING
+}
---
+++ PartyMember.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @author Mostafa Mohamed Ali
+ */
+class PartyMember implements RegisteredUser{
+
+    private String firstName() {
+        return firstname;
+    }
+
+    private String lastName() {
+        return lastName;
+    }
+
+    private int age() {
+        return age;
+    }
+
+    private boolean isAdult() {
+        if(age >= 18) {
+            return isAdult;
+        }
+    }
+
+    public PartyMember(String firstName, String lastName, int age, boolean isAdult) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.isAdult = isAdult;
+    }
+}
