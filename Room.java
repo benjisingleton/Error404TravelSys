d4630fd13d11b4a2f62c372dad845dbd1e5bd254
MostafaMohAli <MOSTAFA@email.sc.edu>
2022-03-15 12:43:53 -0400

    update

---
+++ Room.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Mostafa Mohamed Ali
+ */
+
+ private ArrayList<RoomAmenity> roomAmenities;
+ private int floorNumber;
+ private int roomNumber;
+ private int capacity;
+ private Reservation roomReservation;
+
+ public Room (ArrayList<RoomAmenity> roomAmenities, int floorNumber, int roomNumber, int capacity, Reservation roomReservation) {
+     
+ }
