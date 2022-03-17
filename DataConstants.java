8122dae43c0ef413745918deb4df934eee1dbe07
benji <bencs521@gmail.com>
2022-03-17 12:49:01 -0400

    starting on data loader and fixing json files

---
+++ DataConstants.java
@@ -0,0 +1,11 @@
+
+public abstract class DataConstants {
+	protected static final String FLIGHTS_FILE_NAME = "flights.json";
+	protected static final String PRICE_OF_FLIGHT = "priceOfFlight";
+	protected static final String AIRLINE = "airline";
+	protected static final String DEPT_AIRPORT = "deptAirport";
+    protected static final String ARRIV_AIRPORT = "arrivAirport";
+    protected static final String TIME_OF_DEPT = "timeOfDeparture";
+    protected static final String TIME_OF_ARRIV = "timeOfArrival";
+    protected static final String SEATS = "seats";
+}
---
+++ DataLoader.java
@@ -0,0 +1,40 @@
+/**
+ * Data loader for json files
+ * @author Benjamin Singleton
+ * Following Portia's data loader
+ */
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;;
+
+public class DataLoader extends DataConstants{
+	
+	public static ArrayList<Person> loadPeople() {
+		ArrayList<Person> people = new ArrayList<Person>();
+		
+		try {
+			FileReader reader = new FileReader(PEOPLE_FILE_NAME);
+			JSONParser parser = new JSONParser();
+			JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+			
+			for(int i=0; i < peopleJSON.size(); i++) {
+				JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+				String firstName = (String)personJSON.get(PEOPLE_FIRST_NAME);
+				String lastName = (String)personJSON.get(PEOPLE_LAST_NAME);
+				String phoneNumber = (String)personJSON.get(PEOPLE_PHONE_NUMBER);
+				
+				people.add(new Person(firstName, lastName, phoneNumber));
+			}
+			
+			return people;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
