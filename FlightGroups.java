ef60e67b8cc7d91bb9bb2940a448ee6964841dfd
benji <bencs521@gmail.com>
2022-03-24 12:24:09 -0400

    flight groups and adding check if full flight is available

--- FlightGroups.java
+++ FlightGroups.java
@@ -1,8 +1,29 @@
 import java.util.ArrayList;
 
-public class FlightGroups {
+/**
+ * Class to specify SearchBehavior for
+ * multicity and roundtrip flights
+ * @author Benjamin Singleton
+ */
+
+public class FlightGroups implements SearchBehavior {
 
     private ArrayList<Flight> allFlights = new ArrayList<>();
     
-    
+    public void configureSearch() {
+
+    }
+
+    public void displaySearch() {
+
+    }
+
+    public boolean checkAvailabilty(int numofSeats) {
+        return true;
+    }
+
+    public boolean checkCapacity(int numInParty) {
+        return true;
+    }
+
 }
