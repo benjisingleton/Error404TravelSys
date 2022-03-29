45e1a5d2bb843320dd1957946e5e8ea242f3f507
danielhenington <danhenington@gmail.com>
2022-03-29 13:25:06 -0400

    javadoc

--- UUID_Gen.java
+++ UUID_Gen.java
@@ -1,7 +1,14 @@
 import java.util.UUID;
-
+/**
+ * @author rcd1
+ * Generates a UUID 
+ */
 public class UUID_Gen {
-    public static void main(String[] args) {
+    /**
+     * Generates a UUID
+     * @param args an array of sequence characters
+     */
+        public static void main(String[] args) {
         UUID newID = UUID.randomUUID();
         System.out.println("Your ID: " + newID);   
     }
