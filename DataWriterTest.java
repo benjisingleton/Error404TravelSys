7f45aaadacc99ce24b39fd3b3d719e1b18653972
MostafaMohAli <MOSTAFA@email.sc.edu>
2022-04-12 19:41:01 -0400

    DataWriterTest

--- DataWriterTest.java
+++ DataWriterTest.java
@@ -19,11 +19,65 @@ class DataWriterTest {
 	//assertNotNull(val)
 	
 	
+	private Users user = Users.getInstance();
+	private ArrayList<User> userList = Users.getRegisteredUsers();
+
+	@BeforeEach
+	public void setup() {
+		Users.getInstance().getRegisteredUsers().clear();
+		DataWriter.saveUsers();
+	}
+
+	@AfterEach
+	public void tearDown() {
+		Users.getInstance().getRegisteredUsers().clear();
+		DataWriter.saveUsers();
+	}
 	
-	
+	@Test
+	void tesWritingZeroUsers() {
+		userList = DataLoader.get.Users():
+		assertEquals(0, userList.size());
+	}
+
 	@Test
 	public void saveUsers() {
 		assertEquals(true, true);
 	}
+
+	@Test
+	void testWritingOneUser() {
+		userList.add(new User("tired", "Ryan", "Davis", 20));
+		DataWriter.saveUsers();
+		assertEquals("tired", DataLoader.getUsers().get(0).getUserName());
+	}
+	
+	@Test
+	void testWritingFiveUsers() {
+		userList.add(new User("tired", "Ryan", "Davis", 20));
+		userList.add(new User("tiredd", "Ryan", "Davis", 20));
+		userList.add(new User("tireddd", "Ryan", "Davis", 20));
+		userList.add(new User("tiredddd", "Ryan", "Davis", 20));
+		userList.add(new User("tireddddd", "Ryan", "Davis", 20));
+		DataWriter.saveUsers();
+		assertEquals("tiredd", DataLoader.getUsers().get(2).getUserName());
+	}
+
+	@Test
+	void testWritingEmptyUser() {
+		userList.add(new User("", "", "", 0, ""));
+		DataWriter.saveUsers();
+		assertEquals("", DataLoader.getUsers().get(0).getUserName());
+	}
+	
+	@Test
+	void testWritingNullUser() {
+		userList.add(new User(null, "", "", 0, ""));
+		DataWriter.saveUsers();
+		assertEquals(null, DataLoader.getUsers().get(0).getUserName());
+	}
+
+	
+	
 	
 }
