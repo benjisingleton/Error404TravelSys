6e67c6777f5fd214d75082f73100f879bba57ba4
benji <bencs521@gmail.com>
2022-04-12 19:00:57 -0400

    tests made

---
+++ HotelsTest.java
@@ -0,0 +1,29 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HotelsTest {
+
+	
+	//assertEquals(val1,val2)
+	//assertFalse(val)
+	//assertTrue(val)
+	//assertSame(val1,val2)
+	//assertNotSame(val1,val2)
+	//assertNull(val)
+	//assertNotNull(val)
+	
+	
+	
+	
+	@Test
+	public void getInstance() {
+		assertEquals(true, true);
+	}
+	
+}
