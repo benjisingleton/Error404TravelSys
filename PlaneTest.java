ee288b7cb322743e401e4e72462f3bc131184e0b
benji <bencs521@gmail.com>
2022-04-12 19:03:15 -0400

    all tests made

---
+++ PlaneTest.java
@@ -0,0 +1,26 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlaneTest {
+
+	
+	//assertEquals(val1,val2)
+	//assertFalse(val)
+	//assertTrue(val)
+	//assertSame(val1,val2)
+	//assertNotSame(val1,val2)
+	//assertNull(val)
+	//assertNotNull(val)
+	
+	
+	
+	
+	
+	
+}
