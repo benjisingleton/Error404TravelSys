80181778029db1d5a60f959a456243d9cbe27f11
MostafaMohAli <MOSTAFA@email.sc.edu>
2022-04-12 21:14:57 -0400

    users test

--- UsersTest.java
+++ UsersTest.java
@@ -8,6 +8,39 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UsersTest {
+	private Users users = Users.getInstance();
+	private ArrayList<RegisteredUser> userList = users.getRegisteredUsers();
+
+	@BeforeEach
+	public void setup() {
+		userList.clear();
+		userList.add(new RegisteredUser("rDavis", "Ryan", "Davis", 20));
+		DataWriter.saveUsers();
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		Users.getInstance().getUsers().clear();
+		DataWriter.saveUsers();
+	}
+
+	@Test
+	void testHaveUserValidFirstItem() {
+		boolean hasAmy = users.haveUser("rDavis");
+		assertTrue(hasRyan);
+	}
+
+	@Test
+	void testHaveUserEmpty() {
+		boolean hasEmpty = users.haveUser("");
+		assertFalse(hasEmpty);
+	}
+	
+	@Test
+	void testHaveUserNull() {
+		boolean hasNull = users.haveUser(null);
+		assertFalse(hasNull);
+	}
 	
 	
 }
