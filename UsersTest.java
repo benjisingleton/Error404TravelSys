fb7ec0e7e53502d9f1f367e40aae08650a4e2080
benji <bencs521@gmail.com>
2022-04-12 21:22:13 -0400

    fixeddd

--- UsersTest.java
+++ UsersTest.java
@@ -1,6 +1,9 @@
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterEach;
@@ -11,35 +14,39 @@ class UsersTest {
 	private Users users = Users.getInstance();
 	private ArrayList<RegisteredUser> userList = users.getRegisteredUsers();
 
+    private UUID userID = new UUID(0,1);
+    private RegistrationInfo userInfo = new RegistrationInfo("Ryan", "Davis", "rDavis", "password", 20, true);
+    private BookingList savedBookings = new BookingList();
+    private ArrayList<PartyMember> partyMembers = new ArrayList<>();
+
+
+
 	@BeforeEach
 	public void setup() {
 		userList.clear();
-		userList.add(new RegisteredUser("rDavis", "Ryan", "Davis", 20));
+		userList.add(new RegisteredUser(userID, userInfo, savedBookings, partyMembers));
 		DataWriter.saveUsers();
 	}
 	
 	@AfterEach
 	public void tearDown() {
-		Users.getInstance().getUsers().clear();
+		Users.getInstance().getRegisteredUsers().clear();
 		DataWriter.saveUsers();
 	}
 
 	@Test
 	void testHaveUserValidFirstItem() {
-		boolean hasAmy = users.haveUser("rDavis");
-		assertTrue(hasRyan);
+		assertEquals(true, Users.getInstance().getRegisteredUsers().get(0).getUserInfo().getFirstName());
 	}
 
 	@Test
 	void testHaveUserEmpty() {
-		boolean hasEmpty = users.haveUser("");
-		assertFalse(hasEmpty);
+		// TO DO
 	}
 	
 	@Test
 	void testHaveUserNull() {
-		boolean hasNull = users.haveUser(null);
-		assertFalse(hasNull);
+		// TO DO
 	}
 	
 	
